349a894c5db2f15109f23dc9d5f03868e4697526
shih.s <shih.s@tc-gaming.com>
2023-08-02 12:46:50 +0800

    first commit

---
+++ src/test/java/bf/bfcs/BfcsApplicationTests.java
@@ -0,0 +1,13 @@
+package bf.bfcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
